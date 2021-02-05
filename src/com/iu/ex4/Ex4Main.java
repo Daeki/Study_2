112ad1d88558c5a39c0fe2fad6c15398d90b012e
Daeki <kimdaeki@gmail.com>
2021-02-05 17:23:52 +0900

    11

--- src/com/iu/ex4/Ex4Main.java
+++ src/com/iu/ex4/Ex4Main.java
@@ -5,26 +5,29 @@ public class Ex4Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// test 용도
-		MemberService memberService = new MemberService();
-		Member [] members = memberService.memberJoin();
+//		MemberService memberService = new MemberService();
+//		Member [] members = memberService.memberJoin();
+//		
+//		for(int i=0;i<members.length;i++) {
+//			System.out.println(members[i].id);
+//			System.out.println(members[i].pw);
+//			System.out.println(members[i].name);
+//		}
+//		
+//		//memberLogin 시도
+//		//로그인성공하면 000 님 로그인성공 출력
+//		//로그인실패하면 로그인 실패 출력
+//		//String name = memberService.memberLogin(members);
+//		//int index = memberService.memberLogin(members);
+//		Member member = memberService.memberLogin(members);
+//		if(member != null) {
+//			System.out.println(member.name+" 님 로그인 성공");
+//		}else {
+//			System.out.println("로그인 실패");
+//		}
 		
-		for(int i=0;i<members.length;i++) {
-			System.out.println(members[i].id);
-			System.out.println(members[i].pw);
-			System.out.println(members[i].name);
-		}
-		
-		//memberLogin 시도
-		//로그인성공하면 000 님 로그인성공 출력
-		//로그인실패하면 로그인 실패 출력
-		//String name = memberService.memberLogin(members);
-		//int index = memberService.memberLogin(members);
-		Member member = memberService.memberLogin(members);
-		if(member != null) {
-			System.out.println(member.name+" 님 로그인 성공");
-		}else {
-			System.out.println("로그인 실패");
-		}
+		MemberController memberController = new MemberController();
+		memberController.start();
 		
 	}
 
--- src/com/iu/ex4/MemberController.java
+++ src/com/iu/ex4/MemberController.java
@@ -1,12 +1,44 @@
 package com.iu.ex4;
 
+import java.util.Scanner;
+
 public class MemberController {
 	
 	
 	//메서드명 : start
-	//memberJoin 실행
+	//memberJoin 실행(호출)
 	// 1. 로그인
 	//
 	// 2. 종료
+	public void start() {
+		Scanner sc = new Scanner(System.in);
+		MemberService memberService = new MemberService();
+		Member [] members = memberService.memberJoin();
+		
+		boolean check =true;
+		
+		while(check) {
+			System.out.println("1. 로그인");
+			System.out.println("2. 종  료");
+			int select = sc.nextInt();
+			
+			if(select==1) {
+				Member member= memberService.memberLogin(members);
+				if(member != null) {
+					System.out.println(member.name+" 로그인성공");
+					break;
+				}else {
+					System.out.println("로그인 실패");
+				}
+				
+			}else {
+				System.out.println("프로그램 종료");
+				break;
+			}
+			
+		}
+		
+		
+	}
 
 }
